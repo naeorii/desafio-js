83fa2f8d875d38daca7c3b949464695dce4f8214
Taiana dos Santos <136939790+naeorii@users.noreply.github.com>
2026-01-05 10:54:34 -0300

    Create index3.js

---
+++ index3.js
@@ -0,0 +1,31 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque = "";
+
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+    }
+
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const heroi = new Heroi("Arthas", 30, "mago");
+heroi.atacar();
